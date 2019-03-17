95d27d34a702445ea1c68206c9b5068d63b7557a
Robert Wood Jr <rdrwood@gmail.com>
2019-03-16 21:03:12 -0700

    started to shuffle deck

--- FinalExamCPP/Main.cpp
+++ FinalExamCPP/Main.cpp
@@ -77,12 +77,16 @@ void printMenu() {
 
 // This method will display the deck of cards
 void displayDeck(array<const string *, DECK_SIZE> deck) {
-	// TO DO: REPLACE THE BODY OF THIS FUNCTION
 	cout << "This is the deck of Cards" << endl << endl;
+
 }
 
 // This method will shuffle the deck of cards
 void shuffleDeck(array<const string *, DECK_SIZE> * deck) {
-	// TO DO: REPLACE THE BODY OF THIS FUNCTION
+	// using the default random number generator will produce random  0 - 51
+	static default_random_engine engine(static_cast<unsigned int>(time(0)));
+	static uniform_int_distribution<unsigned int> randomInt(0, 51);
+	
+	// Tells the user the deck is shuffled
 	cout << "Shuffled the deck!" << endl << endl;
 }
